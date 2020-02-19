4505759b86659dea33115965a37fb54bfe7a3a5f
milad ahmadi <miladahmadi803@gmail.com>
2020-02-19 18:15:58 +0330

    gitignore

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const { WebClient } = require('@slack/web-api');
-const { createEventAdapter } = require('@slack/events-api');
-const { RTMClient } = require('@slack/rtm-api');
-
-const rtm = new RTMClient(token);
-
 const express = require("express");
 const app = express();
-const slackSigningSecret = 'bb615790232e67387c61160aa98fd760';
 const port = process.env.PORT || 3000;
-const router = express.Router();
-const web = new WebClient(token);
 const bodyParser = require("body-parser");
 const slackRouter = require("./slack");
 const http = require('http');
@@ -18,41 +9,17 @@ const http = require('http');
 const server = http.createServer(app);
 
 server.listen(port);
-//// Initialize the adapter to trigger listeners with envelope data and headers
-//const slackEvents = createEventAdapter(slackSigningSecret, {
-//  includeBody: true,
-//  includeHeaders: true,
-//});
-//
-//// Listeners now receive 3 arguments
-//slackEvents.on('message', (event, body, headers) => {
-//  console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
-//  console.log(`The event ID is ${body.event_id} and time is ${body.event_time}`);
-//  if (headers['X-Slack-Retry-Num'] !== undefined) {
-//    console.log(`The delivery of this event was retried ${headers['X-Slack-Retry-Num']} times because ${headers['X-Slack-Retry-Reason']}`);
-//  }
-//});
-rtm.on('message', (event) => {
-  console.log('event',event);
-});
 
-(async () => {
-  await rtm.start();
-})();
+
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json({limit: '50mb'}));
 
 (async () => {
-//  const server = await slackEvents.start(port);
   console.log(`Listening for events on ${port}`);
 })();
 
 app.use("/api", slackRouter);
 
-
-
-// The current date
-const currentTime = new Date().toTimeString();
 
 (async () => {
 
--- slack.js
+++ slack.js
@@ -1,8 +1,32 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const { WebClient } = require('@slack/web-api')
+const config = require('./config')
+const axios = require('axios')
+const BotMessages = ['Hey! Are you ready to have our YapAiTek daily meeting now?(y/n)',
+  'Nice, this is your update for YapAiTek.Let\'s begin! What did you work on yesterday?',
+  'Ok. What are you going to work on today?',
+  'Great. Do you have any blockers? If so, just tell me. Otherwise please say \'no\'.',
+  'Well done! This is all, you can continue with your work ']
+const web = new WebClient(config.token)
 
-router.post("/message",function (req,res) {
-  console.log(req);
-});
+router.post('/message', function (req, res) {
+  if (req.body.event.channel_type === 'im' && req.body.event.type === 'message') {
+    axios.get(`https://slack.com/api/conversations.history?token=${config.token}&channel=${req.body.event.channel}&limit=10`).then(async messages => {
+      let lastBotMessage = null
+      for (let item of messages.data.messages) {
+        if (item.user !== req.body.event.channel_type) {
+          lastBotMessage = item.text
+          break
+        }
+      }
+      await web.chat.postMessage({
+        channel:req.body.event.channel ,
+        text: lastBotMessage?BotMessages[BotMessages.indexOf(lastBotMessage)+1]:BotMessages[0],
+      })
 
-module.exports = router;
+    })
+  }
+})
+
+module.exports = router
