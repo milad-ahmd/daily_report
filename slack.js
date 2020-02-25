76652e4f8695cece6f9f50c434c6373bb803b0ba
milad ahmadi <miladahmadi803@gmail.com>
2020-02-25 08:01:37 +0330

    daily bot

--- slack.js
+++ slack.js
@@ -13,144 +13,99 @@ const BotMessages = ['Hey! Are you ready to have our YapAiTek daily meeting now?
   'Ok. What are you going to work on today?',
   'Great. Do you have any blockers? If so, just tell me. Otherwise please say \'no\'.',
   'Well done! This is all, you can continue with your work ']
+
 const web = new WebClient(config.token)
+
+exports.sendMessage = async (userId, userSlackId, text, question, channel) => {
+  await web.chat.postMessage({
+    channel: channel,
+    text: text,
+  })
+  const message = new Message({
+    _id: new mongoose.Types.ObjectId(),
+    text: text,
+    question: question,
+    userId: userId,
+    userSlackId: userSlackId,
+    channelId: channel,
+    date: moment(new Date()).format('YYYY/MM/DD')
+  })
+  message.save().then(result => {
+  }).catch(err => {
+    console.log(err)
+  })
+}
 
 router.post('/message', function (req, res) {
   if (req.body.event.channel_type === 'im' && req.body.event.type === 'message') {
-    console.log(req.body.event);
+    console.log(req.body.event)
     axios.get(`https://slack.com/api/conversations.history?token=${config.token}&channel=${req.body.event.channel}&limit=10`).then(async messages => {
-      User.find({slackId:req.body.event.user}).then(async docs=>{
-        if(docs.length>0){
+      User.find({ slackId: req.body.event.user }).then(async docs => {
+        if (docs.length > 0&&!req.body.event.bot_id) {
           let lastBotMessage = null
-          let lastMessage = messages.data.messages?messages.data.messages[0].text:null;
+          let lastMessage = messages.data.messages ? messages.data.messages[0].text : null
           let lastBotMessageIndex = 0
-          let i=0;
+          let i = 0
 
           for (let item of messages.data.messages) {
-            i++;
-            if (item.bot_id==='BU4HJ558X'&&!lastBotMessage) {
+            i++
+            if (item.bot_id && !lastBotMessage) {
               lastBotMessage = item.text
-              lastBotMessageIndex=i-1;
+              lastBotMessageIndex = i - 1
               break
             }
           }
-          if(lastBotMessageIndex!==0){
-            if(lastMessage&&lastMessage==='daily'){
+
+          if (lastBotMessageIndex !== 0) {
+            if (lastMessage && lastMessage === 'daily') {
               await web.chat.postMessage({
-                channel:req.body.event.channel ,
+                channel: req.body.event.channel,
                 text: BotMessages[0],
               })
-              const message = new Message({
-                _id: new mongoose.Types.ObjectId(),
-                text: BotMessages[0],
-                question: lastMessage,
-                userId: docs[0]._id,
-                userSlackId: req.body.event.user,
-                channelId: req.body.event.channel,
-                date: moment(new Date()).format('YYYY/MM/DD')
-              })
-              const message1 = new Message({
-                _id: new mongoose.Types.ObjectId(),
-                text: lastMessage,
-                question: 'daily',
-                userId: docs[0]._id,
-                userSlackId: req.body.event.user,
-                channelId: req.body.event.channel,
-                date: moment(new Date()).format('YYYY/MM/DD')
-              })
-              message.save().then(result => {
-              }).catch(err => {
-                console.log(err)
-              })
-              message1.save().then(result => {
-              }).catch(err => {
-                console.log(err)
-              })
-            }else{
-
-              await web.chat.postMessage({
-                channel:req.body.event.channel,
-                text: lastBotMessage?BotMessages[BotMessages.indexOf(lastBotMessage)+1]:BotMessages[0],
-              })
-              const message = new Message({
-                _id: new mongoose.Types.ObjectId(),
-                text: lastMessage,
-                question: lastBotMessage,
-                userId: docs[0]._id,
-                userSlackId: req.body.event.user,
-                channelId: req.body.event.channel,
-                date: moment(new Date()).format('YYYY/MM/DD')
-              })
-              const message1 = new Message({
-                _id: new mongoose.Types.ObjectId(),
-                text: lastBotMessage?BotMessages[BotMessages.indexOf(lastBotMessage)+1]:BotMessages[0],
-                question: lastMessage,
-                userId: docs[0]._id,
-                userSlackId: req.body.event.user,
-                channelId: req.body.event.channel,
-                date: moment(new Date()).format('YYYY/MM/DD')
-              })
-              message.save().then(result => {
-              }).catch(err => {
-                console.log(err)
-              })
-              message1.save().then(result => {
-              }).catch(err => {
-                console.log(err)
-              })
-
-              if(BotMessages.indexOf(lastBotMessage)+1===BotMessages.length-1){
-                await web.chat.postMessage({
-                  channel:'#daily',
-                  text: `daily report complete by <${req.body.event.user}>`,
+            } else {
+              let botMessageIndex = lastBotMessage ? BotMessages.indexOf(lastBotMessage) + 1 : 0
+              if (botMessageIndex === 1 && lastMessage === 'n') {
+                return await web.chat.postMessage({
+                  channel: req.body.event.channel,
+                  text: 'ok you can complete your daily report if you have been ready with send daily word to bot!.',
                 })
               }
+              await sendMessage(docs[0]._id,req.body.event.user,BotMessages[botMessageIndex],botMessageIndex===0?'daily':BotMessages[botMessageIndex]);
+              if (botMessageIndex === BotMessages.length - 1) {
+                Message.find({userId:docs[0]._id,userSlackId:req.body.event.user,channelId:req.body.event.channel,date:moment(new Date()).format('YYYY/MM/DD')}).then(async reports=>{
+                  var text='';
+                  for(let item of reports){
+                    text+=item.question+'\n'
+                    text+=item.text+'\n'
+                  }
+                  text+='author: '+`#<${req.body.event.user}>`
+                  await web.chat.postMessage({
+                    channel: '#daily',
+                    text: text,
+                  })
+                })
+
+              }
             }
-          }else{
-            if(lastMessage&&lastMessage==='daily'){
+          } else {
+            if (lastMessage && lastMessage === 'daily') {
               await web.chat.postMessage({
-                channel:req.body.event.channel ,
+                channel: req.body.event.channel,
                 text: BotMessages[0],
-              })
-              const message = new Message({
-                _id: new mongoose.Types.ObjectId(),
-                text: BotMessages[0],
-                question: BotMessages[0],
-                userId: docs[0]._id,
-                userSlackId: req.body.event.user,
-                channelId: req.body.event.channel,
-                date: moment(new Date()).format('YYYY/MM/DD')
-              })
-              message.save().then(result => {
-              }).catch(err => {
-                console.log(err)
               })
             }
           }
-        }else if(!req.body.event.bot_id){
+        } else if (!req.body.event.bot_id) {
           const user = new User({
             _id: new mongoose.Types.ObjectId(),
-            slackId:req.body.event.user,
-            channelId:req.body.event.channel,
+            slackId: req.body.event.user,
+            channelId: req.body.event.channel,
             isActive: true
           })
           user.save().then(async result => {
             await web.chat.postMessage({
-              channel:req.body.event.channel ,
+              channel: req.body.event.channel,
               text: BotMessages[0],
-            })
-            const message = new Message({
-              _id: new mongoose.Types.ObjectId(),
-              text: 'Hey! Are you ready to have our YapAiTek daily meeting now?(y/n)',
-              question: 'Hey! Are you ready to have our YapAiTek daily meeting now?(y/n)',
-              userId: user._id,
-              userSlackId: req.body.event.user,
-              channelId: req.body.event.channel,
-              date: moment(new Date()).format('YYYY/MM/DD')
-            })
-            message.save().then(result => {
-            }).catch(err => {
-              console.log(err)
             })
           }).catch(err => {
             console.log(err)
@@ -158,8 +113,6 @@ router.post('/message', function (req, res) {
           })
         }
       })
-
-
 
     })
   }
