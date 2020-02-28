fc452517633885f297d3ff3b5395117b017db607
milad ahmadi <miladahmadi803@gmail.com>
2020-02-28 13:58:05 +0330

    daily bot

--- jobs/daily.js
+++ jobs/daily.js
@@ -1,46 +1,52 @@
-const cron = require('node-cron')
 const express = require('express')
 const { WebClient } = require('@slack/web-api')
 const config = require('../config')
 app = express()
 const User = require('../models/user')
-const mongoose = require('mongoose')
 const Message = require('../models/message')
 const moment = require('moment')
 
 const web = new WebClient(config.token)
 
-var schedule = require('node-schedule-tz');
 
-schedule.scheduleJob('0 9 * * *','Asia/Tehran', function(){
-  User.find({ isActive: true }).then(async docs => {
-    if (docs.length > 0) {
-      Message.find({date:moment(new Date()).format('YYYY/MM/DD'),complete:true}).then(async messages=>{
-        let usersComplete=[];
-        let users=[]
-        let usersSlackId=[]
-        if(messages){
-          for(let sub of messages){
-            usersComplete.push(sub.userId)
-          }
-          for (let item of docs) {
-            if(!(usersComplete.indexOf(item._id)>-1)){
-              users.push(item);
-              usersSlackId.push(item.slackId);
+
+const CronJob = require('cron').CronJob;
+const job = new CronJob({
+  // Run at 05:00 Central time, only on weekdays
+  cronTime: '00 00 09 * * *',
+  onTick: function() {
+    User.find({ isActive: true }).then(async docs => {
+      if (docs.length > 0) {
+        Message.find({date:moment(new Date()).format('YYYY/MM/DD'),complete:true}).then(async messages=>{
+          let usersComplete=[];
+          let users=[]
+          let usersSlackId=[]
+          if(messages){
+            for(let sub of messages){
+              usersComplete.push(sub.userId)
+            }
+            for (let item of docs) {
+              if(!(usersComplete.indexOf(item._id)>-1)){
+                users.push(item);
+                usersSlackId.push(item.slackId);
+              }
+            }
+            for (let item of docs) {
+              await web.chat.postMessage({
+                channel: item.channelId,
+                text: 'Hey! Are you ready to have our YapAiTek daily meeting now?(y/n)',
+              })
             }
           }
-          for (let item of docs) {
-            await web.chat.postMessage({
-              channel: item.channelId,
-              text: 'Hey! Are you ready to have our YapAiTek daily meeting now?(y/n)',
-            })
-          }
-        }
-      })
-    }
-  }).catch(err => {
-    console.log(err)
-  })
+        })
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  start: true,
+  timeZone: 'Asia/Tehran'
 });
+job.start();
 
 app.listen("3001");
--- jobs/dailyCheck.js
+++ jobs/dailyCheck.js
@@ -1,46 +1,49 @@
-const cron = require('node-cron')
 const express = require('express')
 const { WebClient } = require('@slack/web-api')
 const config = require('../config')
 app = express()
 const User = require('../models/user')
-const mongoose = require('mongoose')
 const Message = require('../models/message')
 const moment = require('moment')
-var schedule = require('node-schedule-tz');
 const web = new WebClient(config.token)
 
-schedule.scheduleJob('0 9 * * *','Asia/Tehran', function(){
-  User.find({ isActive: true }).then(async docs => {
-    if (docs.length > 0) {
-      Message.find({date:moment(new Date()).format('YYYY/MM/DD'),complete:true}).then(async messages=>{
-        let usersComplete=[];
-        let users=[]
-        let usersSlackId=[]
-        if(messages){
-          for(let sub of messages){
-            usersComplete.push(sub.userId)
-          }
-          for (let item of docs) {
-            if(!(usersComplete.indexOf(item._id)>-1)){
-              users.push(item);
-              usersSlackId.push(item.slackId);
+
+const CronJob = require('cron').CronJob;
+const job = new CronJob({
+  // Run at 05:00 Central time, only on weekdays
+  cronTime: '00 00 12 * * *',
+  onTick: function() {
+    User.find({ isActive: true }).then(async docs => {
+      if (docs.length > 0) {
+        Message.find({date:moment(new Date()).format('YYYY/MM/DD'),complete:true}).then(async messages=>{
+          let usersComplete=[];
+          let users=[]
+          let usersSlackId=[]
+          if(messages){
+            for(let sub of messages){
+              usersComplete.push(sub.userId)
+            }
+            for (let item of docs) {
+              if(!(usersComplete.indexOf(item._id)>-1)){
+                users.push(item);
+                usersSlackId.push(item.slackId);
+              }
+            }
+            for (let item of docs) {
+              await web.chat.postMessage({
+                channel: item.channelId,
+                text: 'Hey! Are you ready to have our YapAiTek daily meeting now?(y/n)',
+              })
             }
           }
-          for (let item of docs) {
-            await web.chat.postMessage({
-              channel: item.channelId,
-              text: 'Hey! Are you ready to have our YapAiTek daily meeting now?(y/n)',
-            })
-          }
-        }
-      })
-    }
-  }).catch(err => {
-    console.log(err)
-  })
+        })
+      }
+    }).catch(err => {
+      console.log(err)
+    })
+  },
+  start: true,
+  timeZone: 'Asia/Tehran'
 });
-cron.schedule("* 12 * * *", function() {
-
-});
+job.start();
 app.listen("3002");
